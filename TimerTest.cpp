a84e567a46b382b62c302ec8723dcb9e407c5b4b
Aaron Snyder <103236901+aaron-snyder@users.noreply.github.com>
2024-01-26 23:56:26 -0800

    Timer
    Created a file for testing timer before implementation with main code

---
+++ TimerTest.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <functional>
+
+bool combat = false;
+
+void timer_start(std::function<void(void)> func, unsigned int interval)
+{
+  std::thread([func, interval]()
+  { 
+    while (true)
+    { 
+      auto x = std::chrono::steady_clock::now() + std::chrono::milliseconds(interval);
+      func();
+      std::this_thread::sleep_until(x);
+    }
+  }).detach();
+}
+
+void startCombat()
+{
+  combat = true;
+  std::cout << "Entered combat" << std::endl;
+}
+
+int main()
+{
+  timer_start(do_something, 1000);
+  while (true)
+    ;
+}
